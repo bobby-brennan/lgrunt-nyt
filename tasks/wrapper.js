5a39d6f471237e330f26ef3b1b076bff2cdd0d5e
EC2 Default User <ec2-user@ip-172-31-14-135.us-west-2.compute.internal>
2014-12-25 16:08:05 +0000

    Remove some logging

--- tasks/wrapper.js
+++ tasks/wrapper.js
@@ -11,11 +11,7 @@
 module.exports = function (grunt) {
   grunt.registerTask('nyt_wrapper', 'A grunt plugin for generating wrappers the NYTimes API', function () {
     var config = grunt.config.get('nyt_wrapper');
-    console.log('conf:' + JSON.stringify(config));
-    console.log('swag file:' + __dirname + '/../swagger/swagger.json');
     config.swagger = grunt.file.readJSON(__dirname + '/../swagger/swagger.json');
-    console.log('swag:' + JSON.stringify(config.swagger));
-    console.log('in dir:' + __dirname);
     grunt.file.expand(__dirname + '/../node_modules/lucy-rest-api-client/tasks').forEach(function(inpt) {console.log('tsk:' + inpt); grunt.loadTasks(inpt)});
     grunt.config('rest_api_client', {default_config: config});
     grunt.task.run('rest_api_client');
